c83c349356265e9b9528c9d6e520deaaf8c51da9
Udalov Igor <i.udalov@rambler-co.ru>
2023-09-03 20:27:02 +0300

    Rubocop

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,6 @@
 
 module ApplicationHelper
   def render_turbo_stream_flash_messages
-    turbo_stream.prepend "flash", partial: "layouts/flash"
+    turbo_stream.prepend 'flash', partial: 'layouts/flash'
   end
 end
